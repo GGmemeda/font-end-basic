425982be97c95d28d13a0bdbad8fb100d05d97ee
caobin <493224687@qq.com>
2018-12-21 18:09:44 +0800

    进制转换

---
+++ exchange/index.js
@@ -0,0 +1,10 @@
+parseInt(num,8);   //八进制转十进制
+parseInt(num,16);   //十六进制转十进制
+parseInt(num).toString(8)  //十进制转八进制
+parseInt(num).toString(16)   //十进制转十六进制
+parseInt(num,2).toString(8)   //二进制转八进制
+parseInt(num,2).toString(16)  //二进制转十六进制
+parseInt(num,8).toString(2)   //八进制转二进制
+parseInt(num,8).toString(16)  //八进制转十六进制
+parseInt(num,16).toString(2)  //十六进制转二进制
+parseInt(num,16).toString(8)  //十六进制转八进制
